4dcabe79baa4c515ca7c9548cac99aec4c078c6d
Anastasiia DOSIAK <adosiak@e1z2r6p5.42.us.org>
2017-04-27 12:52:28 -0700

    mod func added

--- main.c
+++ main.c
@@ -39,6 +39,7 @@ void	ft_view_one(t_data a)
 	printf("Year of birthday: %s\n", a.birthyear);
 	printf("Phone number: %s\n", a.phonenumber);
 	printf("Current project: %s\n", a.curr_proj);
+	printf("\n********************\n");
 }
 
 void	ft_view(FILE *fd)
@@ -76,7 +77,48 @@ void	ft_view(FILE *fd)
 	}
 }
 
+void	ft_mod(FILE *fd)
+{
+	t_data a;
+	int	found;
+	char last_name[50];
 
+	rewind(fd);
+	printf("\nEnter Last name of the student to modify:\n");
+	scanf("%s", last_name);
+
+	while (fread(&a, sizeof(a), 1, fd) == 1)
+	{
+		found = 0;
+		if (strcmp(last_name, a.last_name) == 0)
+		{
+			printf("\n\t%sData of the student you're modifying:%s\n",
+					GREEN BOLD_ON, BOLD_OFF RESET);
+			ft_view_one(a);
+			fseek(fd, -sizeof(t_data), SEEK_CUR);
+			found = 1;
+			break ;
+		}
+	}
+	if (found)
+	{
+		printf("%sEnter new  First Name:%s\n", BOLD_ON, BOLD_OFF RESET);
+		scanf("%s", a.first_name);
+		printf("Enter new Last name:\n");
+		scanf("%s", a.last_name);
+		printf("Enter new Year of birth:\n");
+		scanf("%s", a.birthyear);
+		printf("Enter new Phone number:\n");
+		scanf("%s", a.phonenumber);
+		printf("Enter new Current project:\n");
+		scanf ("%s", a.curr_proj);
+
+		fwrite(&a, sizeof(a), 1, fd);
+	}
+
+	if (found == 0)
+		printf ("There's no such student\n");
+}
 int	main(void)
 {
 	int option;
@@ -92,6 +134,8 @@ int	main(void)
 			ft_add_save(fd);
 		else if (option == 2)
 			ft_view(fd);
+		else if (option == 3)
+			ft_mod(fd);
 		else if (option == 4)
 			ft_del(fd);
 		else if (option == 0)
