face7c6c77e438715fbf6af2a89119cfd8ae3353
Anastasiia DOSIAK <adosiak@e1z2r6p5.42.us.org>
2017-04-27 18:18:45 -0700

    ft_mod.c fixed norm

--- ft_mod.c
+++ ft_mod.c
@@ -6,7 +6,7 @@
 /*   By: adosiak <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/27 18:08:39 by adosiak           #+#    #+#             */
-/*   Updated: 2017/04/27 18:08:50 by adosiak          ###   ########.fr       */
+/*   Updated: 2017/04/27 18:15:06 by adosiak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,14 @@ char	*ft_option(int option)
 	return ("option");
 }
 
+void	ft_mod_option_print(void)
+{
+	printf("\n\t%s%sChoose a field to modify:%s\n\t 1: First name\n\t",
+			CYAN, BOLD_ON, RESET);
+	printf(" 2: Last name\n\t 3: Year of birth\n\t");
+	printf(" 4: Phone number\n\t 5: Current_project\n\t 0: Save\n\n");
+}
+
 void	ft_mod_option(t_data *a, FILE *fd)
 {
 	int option;
@@ -34,13 +42,10 @@ void	ft_mod_option(t_data *a, FILE *fd)
 	option = 1;
 	while (option)
 	{
-		printf("\n\t%s%sChoose a field to modify:%s%s\n\t 1: First name\n\t",
-				CYAN, BOLD_ON, BOLD_OFF, RESET);
-		printf(" 2: Last name\n\t 3: Year of birth\n\t");
-		printf(" 4: Phone number\n\t 5: Current_project\n\t 0: Save\n\n");
+		ft_mod_option_print();
 		scanf("%d", &option);
-		printf("\n%sEnter new %s:%s%s\n",
-				BOLD_ON, ft_option(option), BOLD_OFF, RESET);
+		printf("\n%sEnter new %s:%s\n",
+				BOLD_ON, ft_option(option), RESET);
 		if (option < 0 || option > 5)
 			continue ;
 		if (option == 1)
@@ -65,16 +70,16 @@ void	ft_mod(FILE *fd)
 	char	last_name[50];
 
 	rewind(fd);
-	printf("\n%sEnter Last name of the student to modify:%s%s\n",
-			BOLD_ON, BOLD_OFF, RESET);
+	printf("\n%sEnter Last name of the student to modify:%s\n",
+			BOLD_ON, RESET);
 	scanf("%s", last_name);
 	while (fread(&a, sizeof(a), 1, fd) == 1)
 	{
 		found = 0;
 		if (strcmp(last_name, a.last_name) == 0)
 		{
-			printf("\n\t%sData of the student you're modifying:%s%s\n",
-					BOLD_ON, BOLD_OFF, RESET);
+			printf("\n\t%sData of the student you're modifying:%s\n",
+					BOLD_ON, RESET);
 			ft_view_one(a);
 			fseek(fd, -sizeof(t_data), SEEK_CUR);
 			found = 1;
