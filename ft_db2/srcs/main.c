bd917a4bdb3bdf7906039f6fceacfd1628385f5c
Meo Sum Leung <misyleung@gmail.com>
2017-05-05 18:11:51 -0700

    Did I norm??

--- ft_db2/srcs/main.c
+++ ft_db2/srcs/main.c
@@ -6,19 +6,61 @@
 /*   By: adosiak <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/01 16:33:30 by adosiak           #+#    #+#             */
-/*   Updated: 2017/05/05 16:39:27 by sleung           ###   ########.fr       */
+/*   Updated: 2017/05/05 18:07:35 by sleung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-int		main(void)
+static void	print_options(void)
 {
-	char		db_name[50]; 
+	printf("\n%s-------------------\nChange schema:\n", MAG);
+	printf("-------------------%s\n", RES);
+	printf("[1]: add column\n[2]: delete column\n");
+	printf("\n%s-------------------\nView:\n-------------------%s\n", GRE, RES);
+	printf("[3]: view all\n[4]: search\n");
+	printf("\n%s-------------------\nModify data:\n", CYA);
+	printf("-------------------%s\n", RES);
+	printf("[5]: add new record\n[6]: modify record\n[7]: delete record\n");
+	printf("\n-------------------\n[8]: Clear the screen\n[0]: exit\n");
+}
+
+static void	get_options(int option, t_schema *a, char *db_name, FILE *fd)
+{
+	if (option == 1)
+		add_columns(a, fd, db_name);
+	else if (option == 2)
+		del_column(a, fd, db_name);
+	else if (option == 3)
+		view_all(fd, a);
+	else if (option == 4)
+		search(a, fd);
+	else if (option == 5)
+		add_row(a, fd);
+	else if (option == 6)
+		mod_row(a, fd, db_name);
+	else if (option == 7)
+		del_row(a, fd, db_name);
+	else if (option == 8)
+		clear_screen();
+}
+
+static void	create_schema(t_schema *a, FILE *fd)
+{
+	printf("\n%s%s...Creating schema of new DB%s\n", B_ON, YEL, RES);
+	a->names = 0;
+	a->coloms = 0;
+	get_schema(a, fd);
+	print_schema(a);
+}
+
+int			main(void)
+{
+	char		db_name[50];
 	FILE		*fd;
 	t_schema	a;
 	int			option;
-	int 		file_size;
+	int			file_size;
 
 	option = 42;
 	get_file();
@@ -28,45 +70,14 @@ int		main(void)
 	fseek(fd, 0, SEEK_END);
 	file_size = ftell(fd);
 	if (!file_size)
-	{
-		printf("\n%sCreate schema of new DB%s\n", B_ON, RES);
-		a.names = 0;
-		a.coloms = 0;
-		get_schema(&a, fd);
-		print_schema(&a);
-	}
-
+		create_schema(&a, fd);
 	printf("\n%sWhich option do you want?%s\n", B_ON, RES);
 	a = read_schema(fd);
 	while (option != 0)
 	{
-		printf("\n%s-------------------\nChange schema:\n-------------------%s\n", MAG, RES);
-		printf("[1]: add column\n[2]: delete column\n");
-
-		printf("\n%s-------------------\nView:\n-------------------%s\n", GRE, RES);
-		printf("[3]: view all\n[4]: search\n");
-
-		printf("\n%s-------------------\nModify data:\n-------------------%s\n", CYA, RES);
-		printf("[5]: add new record\n[6]: modify record\n[7]: delete record\n");
-
-		printf("\n-------------------\n[8]: Clear the screen\n[0]: exit\n");
+		print_options();
 		scanf("%i", &option);
-		if (option == 3)
-			view_all(fd, &a);
-		else if (option == 2)
-			del_column(&a, fd, db_name);
-		else if (option == 5)
-			add_row(&a, fd);
-		else if (option == 1)
-			add_columns(&a, fd, db_name);
-		else if (option == 4)
-			search(&a, fd);
-		else if (option == 6)
-			mod_row(&a, fd, db_name);
-		else if (option == 7)
-			del_row(&a, fd, db_name);
-		else if (option == 8)
-			clear_screen();
+		get_options(option, &a, db_name, fd);
 		printf("\n");
 	}
 	return (0);
